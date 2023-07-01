69a7a435006d390d9e9165af7a5c193029a24e23
kenmutesh <kenmutesh901@gmail.com>
2023-07-01 21:07:56 +0300

    login

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+Route::get('/', function (){
     return view('welcome');
 });
+
+Route::get('/login', [\App\Http\Controllers\auth\AuthController::class, 'index'])->name('login');
+Route::get('/register', [\App\Http\Controllers\auth\AuthController::class, 'index'])->name('register');
+Route::get('/password', [\App\Http\Controllers\auth\AuthController::class, 'index'])->name('password.request');
